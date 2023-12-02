a718cd559e94f65016708c0c4f97ab2a8c229d11
ioBroker Adapter Creator <noreply@iobroker.net>
2023-12-02 18:24:37 +0000

    Initial commit by adapter creator

---
+++ admin/words.js
@@ -0,0 +1,43 @@
+/* eslint no-unused-vars: off */
+/* eslint no-global-assign: off */
+/* global systemDictionary */
+"use strict";
+
+systemDictionary = {
+	"srm adapter settings": {
+		"en": "Adapter settings for srm",
+		"de": "Adaptereinstellungen für srm",
+		"ru": "Настройки адаптера для srm",
+		"pt": "Configurações do adaptador para srm",
+		"nl": "Adapterinstellingen voor srm",
+		"fr": "Paramètres d'adaptateur pour srm",
+		"it": "Impostazioni dell'adattatore per srm",
+		"es": "Ajustes del adaptador para srm",
+		"pl": "Ustawienia adaptera dla srm",
+		"zh-cn": "srm的适配器设置"
+	},
+	"User": {
+		"en": "User Name",
+		"de": "User Name",
+		"ru": "User Name",
+		"pt": "User Name",
+		"nl": "User Name",
+		"fr": "User Name",
+		"it": "User Name",
+		"es": "User Name",
+		"pl": "User Name",
+		"zh-cn": "User Name"
+	},
+	"Password": {
+		"en": "Password",
+		"de": "Password",
+		"ru": "Password",
+		"pt": "Password",
+		"nl": "Password",
+		"fr": "Password",
+		"it": "Password",
+		"es": "Password",
+		"pl": "Password",
+		"zh-cn": "Password"
+	}
+};
